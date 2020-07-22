c6352cc214fdb5b8f5b2653f21aa8ba7b2cf2084
Lois Aleghe <loisaleghe@cmail.carleton.ca>
2020-07-22 14:42:39 -0400

    deploying to heroku

--- client/src/utils/api.jsx
+++ client/src/utils/api.jsx
@@ -9,18 +9,18 @@ export default {
   },
 
   getBooks: function () {
-    return axios.get(`${process.env.REACT_APP_API}/books`);
+    return axios.get(`${process.env.REACT_APP_API}/api/books`);
   },
 
   saveBook: function (bookData) {
-    return axios.post(`${process.env.REACT_APP_API}/books`, bookData);
+    return axios.post(`${process.env.REACT_APP_API}/api/books`, bookData);
   },
 
   getBook: function (id) {
-    return axios.get(`${process.env.REACT_APP_API}/books/` + id);
+    return axios.get(`${process.env.REACT_APP_API}/api/books/` + id);
   },
 
   deleteBook: function (id) {
-    return axios.delete(`${process.env.REACT_APP_API}/books/` + id);
+    return axios.delete(`${process.env.REACT_APP_API}/api/books/` + id);
   },
 };
